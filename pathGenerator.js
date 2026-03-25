495d9b04036c1e0820cbbe28017df09620167bd7
sotero <sprm.04@gmail.com>
2026-03-25 21:51:41 +0100

    random path

---
+++ pathGenerator.js
@@ -0,0 +1,40 @@
+import * as THREE from "three";
+
+
+
+
+export function generatePath(){
+    const center =  {x:0,y:1,z:0}
+    const theta_max = Math.random() * Math.PI * 2;
+    const theta_min = (Math.random() * 2 - 1) * Math.PI/2 + theta_max;
+    const r_min = 10
+    const r_max = 50
+
+    const start = new THREE.Vector3(
+        center.x + r_max * Math.cos(theta_max),
+        center.y,
+        center.z + r_max * Math.sin(theta_max)
+    )
+
+    const end = new THREE.Vector3(
+        center.x + r_min * Math.cos(theta_min),
+        center.y,
+        center.z + r_min * Math.sin(theta_min)
+    )
+
+    const middle = new THREE.Vector3().lerpVectors(start, end, 0.5);
+    const offset = 50
+
+    middle.x += (Math.random() - 0.5) * offset;
+    middle.z += (Math.random() - 0.5) * offset;
+
+    const points = [
+        start,
+        middle,
+        end
+    ];
+
+    return new THREE.CatmullRomCurve3(points);
+
+
+}
